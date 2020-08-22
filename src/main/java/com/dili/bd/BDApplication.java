58cc5aeb92df2af1c4ebed9cb1ecb1c754261c5f
shaofan <akhuting@hotmail.com>
2020-08-22 15:04:07 +0800

    * 升级依赖

--- src/main/java/com/dili/bd/BDApplication.java
+++ src/main/java/com/dili/bd/BDApplication.java
@@ -16,7 +16,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = {"com.dili.assets.sdk.rpc", "com.dili.bd.rpc", "com.dili.logger.sdk.rpc"})
-@ComponentScan(basePackages={"com.dili.ss", "com.dili.bd", "com.dili.uap.sdk", "com.dili.logger.sdk"})
+@ComponentScan(basePackages={"com.dili.*"})
 @RestfulScan({"com.dili.bd.rpc","com.dili.uap.sdk.rpc"})
 @DTOScan(value={"com.dili.assets.sdk.dto"})
 public class BDApplication extends SpringBootServletInitializer {

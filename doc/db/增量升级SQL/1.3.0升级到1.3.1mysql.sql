d37e37458e9c4e8512694f85bcd0e7f131d7d190
shaofan <akhuting@hotmail.com>
2020-10-15 15:50:35 +0800

    * 修复品类快捷码可以输入空格

---
+++ doc/db/增量升级SQL/1.3.0升级到1.3.1mysql.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `pos_market` (
+  `creator_id` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '创建人',
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `created_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `market` bigint(20) DEFAULT NULL COMMENT '所属市场',
+  `modify_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `name` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '市场名称',
+  `area` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '所属区域',
+  `notes` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='销地市场';

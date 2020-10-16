55e1c798f8a8854a5816bd81aa515b3bb43eaf31
shaofan <akhuting@hotmail.com>
2020-10-16 15:21:38 +0800

    * 修复销地市场bug

--- src/main/java/com/dili/bd/provider/CityProvider.java
+++ src/main/java/com/dili/bd/provider/CityProvider.java
@@ -2,11 +2,9 @@ package com.dili.bd.provider;
 
 import cn.hutool.json.JSONUtil;
 import com.dili.assets.sdk.dto.CityDto;
-import com.dili.assets.sdk.rpc.CityRpc;
 import com.dili.ss.metadata.FieldMeta;
 import com.dili.ss.metadata.ValuePair;
 import com.dili.ss.metadata.ValueProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -25,9 +23,6 @@ import java.util.Map;
 @Scope("prototype")
 public class CityProvider implements ValueProvider {
 
-    @Autowired
-    private CityRpc cityRpc;
-
 
     @Override
     public List<ValuePair<?>> getLookupList(Object val, Map metaMap, FieldMeta fieldMeta) {
@@ -42,14 +37,12 @@ public class CityProvider implements ValueProvider {
         StringBuilder text = new StringBuilder();
         CityDto cityDto = new CityDto();
 
-        List<CityDto> data = cityRpc.listByExample(cityDto).getData();
         for (Object o : JSONUtil.parseArray(obj.toString())) {
-            Long id = Long.parseLong(o.toString());
-            CityDto temp = data.stream().filter(it -> it.getId().equals(id)).findFirst().get();
+            String name = o.toString();
             if (text.length() == 0) {
-                text.append(temp.getName());
+                text.append(name);
             } else {
-                text.append("/").append(temp.getName());
+                text.append("/").append(name);
             }
         }
 

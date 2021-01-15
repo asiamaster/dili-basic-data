fc61db63c24a8edab92c1fa0d04ec92d28ff52a0
shaofan <akhuting@hotmail.com>
2021-01-15 17:29:50 +0800

    脚本整理

--- doc/db/增量升级SQL/1.3.2升级到2.0.0mysql.sql
+++ doc/db/增量升级SQL/1.3.2升级到2.0.0mysql.sql
@@ -58,3 +58,10 @@ INSERT INTO area_market(market,area) SELECT market_id as market,id as area FROM 
 INSERT INTO `type_market` VALUES (1, 'boutique', null);
 INSERT INTO `type_market` VALUES (2, 'passport', null);
 INSERT INTO `type_market` VALUES (3, 'laborVest', null);
+
+
+ALTER TABLE business_charge_item ADD COLUMN `fund_item` bigint DEFAULT NULL COMMENT '关联的资金项目代码' AFTER charge_type;
+ALTER TABLE business_charge_item ADD COLUMN `fund_item_value` VARCHAR(50) DEFAULT NULL COMMENT '关联的资金项目文本值' AFTER fund_item;
+ALTER TABLE business_charge_item ADD COLUMN `code` VARCHAR(20) DEFAULT NULL COMMENT '费用项编码' AFTER fund_item_value;
+ALTER TABLE business_charge_item ADD COLUMN `fixed` TINYINT DEFAULT NULL COMMENT '是否固定项1-是;0-否' AFTER code;
+update business_charge_item set fixed = 0;
--- doc/db/增量升级SQL/2.0数据迁移.sql
+++ doc/db/增量升级SQL/2.0数据迁移.sql
@@ -1,3 +1,7 @@
 UPDATE `dili-basic-data`.booth_rent br SET type = 1,`user` = (SELECT customer_name FROM dili_ia.assets_lease_order o WHERE o.ID = br.order_id);
 
 UPDATE `dili-basic-data`.assets a SET a.`user` = (SELECT b.`user` from `dili-basic-data`.booth_rent b WHERE a.id = b.booth_id);
+
+ALTER TABLE `dili-basic-data`.`config` AUTO_INCREMENT = 200;
+
+UPDATE `dili-basic-data`.config SET id = id + 100;

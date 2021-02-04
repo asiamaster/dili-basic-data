9ff4c9522a01f65697cc99db9a3c6943df662182
shaofan <akhuting@hotmail.com>
2021-02-04 10:34:23 +0800

    整理脚本

--- doc/db/增量升级SQL/2.0数据迁移.sql
+++ doc/db/增量升级SQL/2.0数据迁移.sql
@@ -2,11 +2,9 @@ UPDATE `dili-basic-data`.booth_rent br SET type = 1,`user` = (SELECT customer_na
 
 UPDATE `dili-basic-data`.assets a SET a.`user` = (SELECT b.`user` from `dili-basic-data`.booth_rent b WHERE a.id = b.booth_id and now() >= b.start and now() <= b.end);
 
-ALTER TABLE `dili-basic-data`.`config` AUTO_INCREMENT = 200;
-
-UPDATE `dili-basic-data`.config SET id = id + 100;
-
 UPDATE `dili-basic-data`.`subject` SET market_id = 8;
+
+TRUNCATE TABLE `dili-basic-data`.config;
 
 INSERT INTO `dili-basic-data`.`config`(id, name, value, notes, state, market_id, type)
 VALUES (1, '现金存款', NULL, NULL, 1, 9, 'fundItem');
@@ -61,10 +59,6 @@ VALUES (25, '检测费', NULL, NULL, 1, 9, 'fundItem');
 INSERT INTO `dili-basic-data`.`config`(id, name, value, notes, state, market_id, type)
 VALUES (26, '查询费', NULL, NULL, 1, 9, 'fundItem');
 INSERT INTO `dili-basic-data`.`config`(id, name, value, notes, state, market_id, type)
-VALUES (27, '冲正', NULL, NULL, 1, 9, 'fundItem');
-INSERT INTO `dili-basic-data`.`config`(id, name, value, notes, state, market_id, type)
-VALUES (28, '冲正园区账户变动金额', NULL, NULL, 1, 9, 'fundItem');
-INSERT INTO `dili-basic-data`.`config`(id, name, value, notes, state, market_id, type)
 VALUES (29, '圈提取款', NULL, NULL, 1, 9, 'fundItem');
 INSERT INTO `dili-basic-data`.`config`(id, name, value, notes, state, market_id, type)
 VALUES (30, '圈提手续费', NULL, NULL, 1, 9, 'fundItem');
@@ -112,3 +106,121 @@ INSERT INTO `dili-basic-data`.`config`(name, value, notes, state, market_id, typ
 VALUES ('更名费', NULL, NULL, 1, 11, 'fundItem');
 INSERT INTO `dili-basic-data`.`config`(name, value, notes, state, market_id, type)
 VALUES ('更型费', NULL, NULL, 1, 11, 'fundItem');
+
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('FtpIP', 'ftp://47.93.63.96/', 'FTP服务器（票据存放）', 1, 8, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('FtpUser', 'ftpuser', 'FTP用户名', 1, 8, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('FtpPwd', 'dili+1qaz=', 'FTP密码', 1, 8, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('DFS', 'https://dfs.diligrp.com', '分布式文件服务器', 1, 8, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('AccessToken', '389af650c1fe4d74a7bbd42c850088a3.1rHkoUqx3d4r3uoyoqpBtw', '分布式文件上传Token', 1, 8, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('MarketName', '山东寿光', '市场名称', 1, 8, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('MarketShort', 'SG', '市场名称简称', 1, 8, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('NoteName', '山东寿光地利农副产品有限公司专用票据', '票据抬头', 1, 8, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('FeeJump', 'OFF', '进门收费跳转开关', 1, 8, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('CodeBound', 'OFF', '进门码单绑定开关', 1, 8, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('DirectPay', 'ON', '进门称重直接交费开关', 1, 8, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('IsAuthorize', 'ON', '回皮授权开关', 1, 8, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('FreePay', 'ON', '免密支付开关', 1, 8, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('CustomerUrl', 'https://customer.diligrp.com', '客户URL', 1, 8, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('ModifyPwdUrl', 'https://uap.diligrp.com/index/changePwd.html', '修改密码Url', 2, 8, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('TradeTypeCode', '006', '理货区编码', 1, 8, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('ContinueReadCardOverTime', '10', '连续读取园区卡超时时间(秒)，设置为0表示不作限制', 1, 8, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('TraceUrl', 'https://trace.nong12.com', '溯源Url', 2, 8, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('BpmcUrl', 'https://bpmc.diligrp.com/task/taskCenter.html', 'BpmcUrl', 2, 8, 'Client');
+
+
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('FtpIP', 'ftp://47.93.63.96/', 'FTP服务器（票据存放）', 1, 9, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('FtpUser', 'ftpuser', 'FTP用户名', 1, 9, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('FtpPwd', 'dili+1qaz=', 'FTP密码', 1, 9, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('DFS', 'https://dfs.diligrp.com', '分布式文件服务器', 1, 8, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('AccessToken', '389af650c1fe4d74a7bbd42c850088a3.1rHkoUqx3d4r3uoyoqpBtw', '分布式文件上传Token', 1, 9, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('MarketName', '沈阳地利', '市场名称', 1, 9, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('MarketShort', 'SY', '市场名称简称', 1, 9, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('NoteName', '沈阳地利农批市场客户电子结算中心专用票据', '票据抬头', 1, 9, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('FeeJump', 'OFF', '进门收费跳转开关', 1, 9, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('CodeBound', 'OFF', '进门码单绑定开关', 1, 9, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('DirectPay', 'ON', '进门称重直接交费开关', 1, 9, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('IsAuthorize', 'ON', '回皮授权开关', 1, 9, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('FreePay', 'ON', '免密支付开关', 1, 9, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('CustomerUrl', 'https://customer.diligrp.com', '客户URL', 1, 9, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('ModifyPwdUrl', 'https://uap.diligrp.com/index/changePwd.html', '修改密码Url', 2, 9, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('TradeTypeCode', '006', '理货区编码', 1, 9, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('ContinueReadCardOverTime', '10', '连续读取园区卡超时时间(秒)，设置为0表示不作限制', 1, 9, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('TraceUrl', 'https://trace.nong12.com', '溯源Url', 2, 9, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('BpmcUrl', 'https://bpmc.diligrp.com/task/taskCenter.html', 'BpmcUrl', 2, 9, 'Client');
+
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('FtpIP', 'ftp://47.93.63.96/', 'FTP服务器（票据存放）', 1, 11, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('FtpUser', 'ftpuser', 'FTP用户名', 1, 11, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('FtpPwd', 'dili+1qaz=', 'FTP密码', 1, 11, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('DFS', 'https://dfs.diligrp.com', '分布式文件服务器', 1, 11, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('AccessToken', '389af650c1fe4d74a7bbd42c850088a3.1rHkoUqx3d4r3uoyoqpBtw', '分布式文件上传Token', 1, 11, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('MarketName', '杭州水产', '市场名称', 1, 11, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('MarketShort', 'HZSC', '市场名称简称', 1, 11, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('NoteName', '杭州昌海实业有限公司专用票据', '票据抬头', 1, 11, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('FeeJump', 'OFF', '进门收费跳转开关', 1, 11, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('CodeBound', 'OFF', '进门码单绑定开关', 1, 11, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('DirectPay', 'ON', '进门称重直接交费开关', 1, 11, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('IsAuthorize', 'ON', '回皮授权开关', 1, 11, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('FreePay', 'ON', '免密支付开关', 1, 11, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('CustomerUrl', 'https://customer.diligrp.com', '客户URL', 1, 11, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('ModifyPwdUrl', 'https://uap.diligrp.com/index/changePwd.html', '修改密码Url', 2, 11, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('TradeTypeCode', '006', '理货区编码', 1, 11, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('ContinueReadCardOverTime', '10', '连续读取园区卡超时时间(秒)，设置为0表示不作限制', 1, 11, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('TraceUrl', 'https://trace.nong12.com', '溯源Url', 2, 11, 'Client');
+INSERT INTO `dili-basic-data`.`config`(`name`, `value`, `notes`, `state`, `market_id`, `type`)
+VALUES ('BpmcUrl', 'https://bpmc.diligrp.com/task/taskCenter.html', 'BpmcUrl', 2, 11, 'Client');
